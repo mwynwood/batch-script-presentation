c083899289616d0b8569585c45e127e00c9da7a8
Marcus Wynwood <mwynwood@gmail.com>
2021-03-29 15:17:56 +1100

    Add files via upload

---
+++ pres.bat
@@ -0,0 +1,373 @@
+:setup
+@echo off
+color B5
+set title=Using Social Media to Engage with Clients and Students
+set page=0
+set line=+------------------------------------------------+-------------------------------------------------+
+echo 
+cls
+
+:cover
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                    Using    
+echo                      _____            _       _   __  __          _ _       
+echo                     / ____^|          (_)     ^| ^| ^|  \/  ^|        ^| (_)      
+echo                    ^| (___   ___   ___ _  __ _^| ^| ^| \  / ^| ___  __^| ^|_  __ _ 
+echo                     \___ \ / _ \ / __^| ^|/ _` ^| ^| ^| ^|\/^| ^|/ _ \/ _` ^| ^|/ _` ^|
+echo                     ____) ^| (_) ^| (__^| ^| (_^| ^| ^| ^| ^|  ^| ^|  __/ (_^| ^| ^| (_^| ^|
+echo                    ^|_____/ \___/ \___^|_^|\__,_^|_^| ^|_^|  ^|_^|\___^|\__,_^|_^|\__,_^|
+echo                                          to Engage with Clients and Students
+echo                                                          with Marcus Wynwood 
+echo.
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:what
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        WHAT IS SOCIAL MEDIA?
+echo.
+echo.
+echo            * Web 1.0
+echo.
+echo            * Web 2.0
+echo.
+echo            * User Generated Content
+echo.
+echo            * Human Interaction
+echo.
+echo. 
+echo.
+echo        SOCIAL MEDIA CAN BE DEFINED AS
+echo.
+echo            "Websites and applications that enable users to create and 
+echo                         share content or to participate in social networking"
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:why
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        WHY USE SOCIAL MEDIA?
+echo.
+echo.
+echo            * Share content
+echo.
+echo            * Build a community
+echo.
+echo            * Be a part of a community
+echo.
+echo            * Because students are already there (?)
+echo.
+echo            * Embed it in your course, ie: part of assessment
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:types
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        TYPES OF SOCIAL MEDIA?
+echo.
+echo.
+echo            * Social Networks
+echo.
+echo            * Photo and Video Sharing
+echo.
+echo            * Blogging
+echo.
+echo            * Micro Blogging
+echo.
+echo            * Messaging
+echo.
+echo            * etc...
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:blooms
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        BLOOMS TAXONOMY
+echo.
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:tools
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        TEACHING TECHNIQUES vs THE TOOLS
+echo.
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:control
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        CONTROLLING SOCIAL MEDIA
+echo.
+echo.
+echo            * Moderating discussion
+echo.
+echo            * What to post
+echo.
+echo            * When to post
+echo.
+echo            * Sponsored Posts
+echo.
+echo            * Privacy Concerns
+echo.
+echo            * When it goes bad...
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:alternatives
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        ALTERNATIVES TO USING SOCIAL MEDIA
+echo.
+echo.
+echo            * Do you really need it at all?
+echo.
+echo            * Tools available in the LMS
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:examples
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo        EXAMPLES OF SOCIAL MEDIA USE
+echo.
+echo.
+echo            * Within the organisation
+echo.
+echo            * Outside the organisation
+echo.
+echo            *
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo            * 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:questions
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                              ___                  _   _                ___ 
+echo                             / _ \ _   _  ___  ___^| ^|_(_) ___  _ __  __^|__ \
+echo                            ^| ^| ^| ^| ^| ^| ^|/ _ \/ __^| __^| ^|/ _ \^| '_ \/ __^|/ /
+echo                            ^| ^|_^| ^| ^|_^| ^|  __/\__ \ ^|_^| ^| (_) ^| ^| ^| \__ \_^| 
+echo                             \__\_\\__,_^|\___^|^|___/\__^|_^|\___/^|_^| ^|_^|___(_) 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+:end
+cls
+set /A page=page+1
+echo.
+echo  %title%                                     Slide %page%
+echo %line%
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                             THANK YOU!
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo %line%
+pause
+
+goto setup
